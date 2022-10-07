46d130667cc1c770ded4f52504f72dabee2ab477
Ankin <83371077+AnkinDa@users.noreply.github.com>
2022-10-07 19:47:19 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,27 +2,53 @@ const contentControlDiv = document.querySelector(".rules-control"),
       ButtonA = document.querySelector(".ca"),
       ButtonB = document.querySelector(".cb"),
       ButtonC = document.querySelector(".cc"),
+      ButtonD = document.querySelector(".cd"),
+      ButtonE = document.querySelector(".ce"),
       ContentA = document.querySelector(".a"),
       ContentB = document.querySelector(".b"),
-      ContentC = document.querySelector(".c");
+      ContentC = document.querySelector(".c"),
+      ContentD = document.querySelector(".d"),
+      ContentE = document.querySelector(".e");
 
 
 ButtonA.addEventListener('click', () => {
     ContentA.classList.replace("hide","show");
     ContentB.classList.replace("show","hide");
     ContentC.classList.replace("show","hide");
+    ContentD.classList.replace("show","hide");
+    ContentE.classList.replace("show","hide");
 })
 
 ButtonB.addEventListener('click', () => {
     ContentA.classList.replace("show","hide");
     ContentB.classList.replace("hide","show");
     ContentC.classList.replace("show","hide");
+    ContentD.classList.replace("show","hide");
+    ContentE.classList.replace("show","hide");
 })
 
 ButtonC.addEventListener('click', () => {
     ContentA.classList.replace("show","hide");
     ContentB.classList.replace("show","hide");
     ContentC.classList.replace("hide","show");
+    ContentD.classList.replace("show","hide");
+    ContentE.classList.replace("show","hide");
+})
+
+ButtonD.addEventListener('click', () => {
+    ContentA.classList.replace("show","hide");
+    ContentB.classList.replace("show","hide");
+    ContentC.classList.replace("show","hide");
+    ContentD.classList.replace("hide","show");
+    ContentE.classList.replace("show","hide");
+})
+
+ButtonE.addEventListener('click', () => {
+    ContentA.classList.replace("show","hide");
+    ContentB.classList.replace("show","hide");
+    ContentC.classList.replace("show","hide");
+    ContentD.classList.replace("show","hide");
+    ContentE.classList.replace("hide","show");
 })
 
 // buttonRuleVRC.addEventListener('click', () => {
